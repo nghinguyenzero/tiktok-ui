31477a3fc03483bcea106ea8f0bab91b09df74fa
NghiNguyen <nghinguyenzero@gmail.com>
2023-11-03 20:39:52 +0700

    add font Tiktok update menu

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -3,14 +3,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faCircleQuestion,
   faCircleXmark,
+  faCloudUpload,
+  faCoins,
   faEarthAsia,
   faEllipsisVertical,
+  faGear,
   faKeyboard,
   faMagnifyingGlass,
+  faSignOut,
   faSpinner,
+  faUser,
 } from '@fortawesome/free-solid-svg-icons'
 import classNames from 'classnames/bind'
-import Tippy from '@tippyjs/react/headless'
+import HeadlessTippy from '@tippyjs/react'
+import Tippy from '@tippyjs/react'
+import 'tippy.js/dist/tippy.css' // optional
 
 import { Wrapper as PopperWrapper } from '~/components/Popper'
 import styles from './Header.module.scss'
@@ -22,7 +29,29 @@ import Menu from '~/components/Popper/Menu'
 const cx = classNames.bind(styles)
 
 const MENU_ITEMS = [
-  { icon: <FontAwesomeIcon icon={faEarthAsia} />, title: 'English' },
+  {
+    icon: <FontAwesomeIcon icon={faEarthAsia} />,
+    title: 'Language',
+    children: {
+      title: 'Language',
+      data: [
+        {
+          type: 'Language',
+          code: 'vi',
+          title: 'Tiếng Việt',
+          children: {
+            title: 'Tiếng Việt',
+            type: 'Tiếng Việt',
+            data: [
+              { code: 'vi1', title: 'Tiếng Việt cơ bản' },
+              { code: 'vi2', title: 'Tiếng Việt nâng cao' },
+            ],
+          },
+        },
+        { type: 'Language', code: 'en', title: 'English' },
+      ],
+    },
+  },
   { icon: <FontAwesomeIcon icon={faCircleQuestion} />, title: 'Feedback and help', to: '/feedback' },
   { icon: <FontAwesomeIcon icon={faKeyboard} />, title: 'Keyboard shortcuts' },
 ]
@@ -32,11 +61,24 @@ function Header(props) {
     setSearchResult([1, 2, 3])
   }, 1000)
 
+  const handleMenuChange = (menuItem) => {
+    console.log('menuItem', menuItem)
+  }
+
+  const currentUser = true
+
+  const userMenu = [
+    { icon: <FontAwesomeIcon icon={faUser} />, title: 'View profile', to: '/@hoa' },
+    { icon: <FontAwesomeIcon icon={faCoins} />, title: 'Get coins', to: '/coin' },
+    { icon: <FontAwesomeIcon icon={faGear} />, title: 'Settings', to: '/settings' },
+    ...MENU_ITEMS,
+    { icon: <FontAwesomeIcon icon={faSignOut} />, title: 'Log out', to: '/logout', separate: true },
+  ]
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
         <img src={images.logo} alt="Tiktok" />
-        <Tippy
+        <HeadlessTippy
           visible={searchResult.length > 0}
           render={(attrs) => (
             <div className={cx('search-result')} tabIndex={1} {...attrs}>
@@ -58,15 +100,37 @@ function Header(props) {
               <FontAwesomeIcon icon={faMagnifyingGlass} />
             </button>
           </div>
-        </Tippy>
-
+        </HeadlessTippy>
         <div className={cx('actions')}>
-          <Button text>Upload</Button>
-          <Button primary>Login</Button>
-          <Menu items={MENU_ITEMS}>
-            <button className={cx('more-btn')}>
-              <FontAwesomeIcon icon={faEllipsisVertical} />
-            </button>
+          {currentUser ? (
+            <>
+              <Tippy delay={[0, 200]} trigger="click" content="Upload video">
+                <button className={cx('action-btn')}>
+                  <FontAwesomeIcon icon={faCloudUpload} />
+                </button>
+              </Tippy>
+              {/* <button className={cx('action-btn')}>
+                <FontAwesomeIcon icon={faMessage} />
+              </button> */}
+            </>
+          ) : (
+            <>
+              <Button text>Upload</Button>
+              <Button primary>Login</Button>
+            </>
+          )}
+          <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange}>
+            {currentUser ? (
+              <img
+                className={cx('user-avatar')}
+                alt="Username"
+                src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tiktok-obj/7048172423539228674.jpeg?x-expires=1699178400&x-signature=6fLGLcapfTyWCWiLZwCO5nqLL60%3D"
+              />
+            ) : (
+              <button className={cx('more-btn')}>
+                <FontAwesomeIcon icon={faEllipsisVertical} />
+              </button>
+            )}
           </Menu>
         </div>
       </div>
