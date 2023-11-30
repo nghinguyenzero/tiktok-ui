ef0ace55103b101552da8b71af1ea09b526e4dad
NghiNguyen <nghinguyenzero@gmail.com>
2023-11-30 21:59:53 +0700

    update icon sidebar

--- src/layouts/Sidebar/Sidebar.js
+++ src/layouts/Sidebar/Sidebar.js
@@ -1,10 +1,38 @@
 import classNames from 'classnames/bind'
 import styles from './Sidebar.module.scss'
+import config from '~/config'
+import Menu, { MenuItem } from './Menu'
+import {
+  HomeActiveIcon,
+  HomeIcon,
+  LiveActiveIcon,
+  LiveIcon,
+  UserGroupActiveIcon,
+  UserGroupIcon,
+} from '~/components/Icons'
 
 const cx = classNames.bind(styles)
 
 function Sidebar(props) {
-  return <aside className={cx('wrapper')}>Sidebar</aside>
+  return (
+    <aside className={cx('wrapper')}>
+      <Menu>
+        <MenuItem
+          title="For you"
+          to={config.routes.home}
+          icon={<HomeIcon />}
+          activeIcon={<HomeActiveIcon />}
+        ></MenuItem>
+        <MenuItem
+          title="Following"
+          to={config.routes.following}
+          icon={<UserGroupIcon />}
+          activeIcon={<UserGroupActiveIcon />}
+        ></MenuItem>
+        <MenuItem title="LIVE" to={config.routes.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon />}></MenuItem>
+      </Menu>
+    </aside>
+  )
 }
 
 export default Sidebar
